9a6040a0967dd1a7b2f057721d84310791db903d
madhesh-maasi <madheshmaasi3@gmail.com>
2021-04-03 18:12:47 +0530

    Maasi Apr - 3

--- src/webparts/adseroTeamsManagement/components/IAdseroTeamsManagementProps.ts
+++ src/webparts/adseroTeamsManagement/components/IAdseroTeamsManagementProps.ts
@@ -6,3 +6,14 @@ export interface IAdseroTeamsManagementProps {
   siteUrl: string;
   graphClient: any;
 }
+
+export interface ICapacityDashBoardProps {
+  description: string;
+  spcontext: WebPartContext;
+  siteUrl: string;
+  graphClient: any;
+  ProfileData:any;
+  landingSwitch:Boolean;
+  pageSwitching:String;
+ 
+}
